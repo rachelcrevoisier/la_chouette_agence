86103f99f738e6964b5359bc9011b4455eb0437e
Rachel Crevoisier <rachelcrevoisier@videotron.ca>
2020-05-31 09:47:13 -0400

    Creation de la page menu

---
+++ includes/menu.php
@@ -0,0 +1,17 @@
+		<nav class="navbar row">
+			<div class="navbar-header">
+				
+				<a class="navbar-brand" href="index.html"><img src="img/la-chouette-agence.png" alt="Logo La Chouette Agence" height="40" /></a>
+
+			</div>
+			<div class="collapse navbar-collapse navbar-1 special-dropdown-nav">
+				<ul class="site-navigation nav navbar-nav">
+					<li>
+						<a href="/index.html">Accueil</a>
+					</li>
+					<li>
+						<a href="/contact.html">Contact &gt;</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
